e5e33e33486d65315deaaf952c6f309396c4ecfe
admhublog-create <adm.hublog@gmail.com>
2026-09-24 12:21:24 -0300

    feat: adiciona componentes de formulario

---
+++ components/ui/input.tsx
@@ -0,0 +1,1 @@
+import * as React from "react";import {cn} from "@/lib/utils";export function Input({className,...props}:React.InputHTMLAttributes<HTMLInputElement>){return <input className={cn("h-10 w-full rounded-lg border bg-white px-3 text-sm outline-none focus:border-[var(--primary)] focus:ring-2 focus:ring-emerald-600/10",className)} {...props}/>}
