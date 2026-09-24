45f6d2836f08a57eed26462e1cdc9a508c1e6204
admhublog-create <adm.hublog@gmail.com>
2026-09-24 14:30:10 -0300

    feat: add tape route

---
+++ app/fita-gomada/page.tsx
@@ -0,0 +1,1 @@
+import {Sidebar} from "@/components/layout/sidebar";import {Header} from "@/components/layout/header";import {TapePage} from "@/components/tape/tape-page";export default function Page(){return <div><Sidebar/><div className="min-h-screen lg:pl-64"><Header/><main className="p-5 lg:p-8"><TapePage/></main></div></div>}
