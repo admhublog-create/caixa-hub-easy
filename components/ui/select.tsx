3a6b07518b6d1186119273c08072686a5b1f57de
admhublog-create <adm.hublog@gmail.com>
2026-09-24 12:27:08 -0300

    feat: add reusable design system component

---
+++ components/ui/select.tsx
@@ -0,0 +1,1 @@
+import * as React from "react";import {cn} from "@/lib/utils";export function Select({className,children,...props}:React.SelectHTMLAttributes<HTMLSelectElement>){return <select className={cn("h-10 w-full rounded-lg border bg-white px-3 text-sm outline-none focus:border-[var(--primary)]",className)} {...props}>{children}</select>}
