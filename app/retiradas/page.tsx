5735360fe27cddc0a9d32994638f0031935f774a
admhublog-create <adm.hublog@gmail.com>
2026-09-24 13:06:41 -0300

    feat: add withdrawals route

---
+++ app/retiradas/page.tsx
@@ -0,0 +1,1 @@
+import {Sidebar} from "@/components/layout/sidebar";import {Header} from "@/components/layout/header";import {WithdrawalsPage} from "@/components/withdrawals/withdrawals-page";export default function Page(){return <div><Sidebar/><div className="min-h-screen lg:pl-64"><Header/><main className="p-5 lg:p-8"><WithdrawalsPage/></main></div></div>}
