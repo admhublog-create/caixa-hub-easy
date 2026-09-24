e677513d2dba33824a8b443683729816d6a8ec82
admhublog-create <adm.hublog@gmail.com>
2026-09-24 15:34:05 -0300

    feat: add reports route

---
+++ app/relatorios/page.tsx
@@ -0,0 +1,1 @@
+import {Sidebar} from "@/components/layout/sidebar";import {Header} from "@/components/layout/header";import {ReportsPage} from "@/components/reports/reports-page";export default function Page(){return <div><Sidebar/><div className="min-h-screen lg:pl-64"><Header/><main className="p-5 lg:p-8"><ReportsPage/></main></div></div>}
