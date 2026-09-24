e85d4ad212e60a02cf4971db12da92e0beeeb740
admhublog-create <adm.hublog@gmail.com>
2026-09-24 15:51:17 -0300

    refactor: use responsive app shell

--- app/caixas/page.tsx
+++ app/caixas/page.tsx
@@ -1,1 +1,1 @@
-import {Sidebar} from "@/components/layout/sidebar";import {Header} from "@/components/layout/header";import {BoxesPage} from "@/components/boxes/boxes-page";export default function Page(){return <div><Sidebar/><div className="min-h-screen lg:pl-64"><Header/><main className="p-5 lg:p-8"><BoxesPage/></main></div></div>}
+import {AppShell} from "@/components/layout/app-shell";import {BoxesPage} from "@/components/boxes/boxes-page";export default function Page(){return <AppShell><main className="p-4 sm:p-5 lg:p-8"><BoxesPage/></main></AppShell>}
