8604f1856c720e5891965b7ab04b377b756a71e1
admhublog-create <adm.hublog@gmail.com>
2026-09-25 08:33:38 -0300

    style: standardize internal page container

--- app/fita-gomada/page.tsx
+++ app/fita-gomada/page.tsx
@@ -1,1 +1,1 @@
-import {AppShell} from "@/components/layout/app-shell";import {TapePage} from "@/components/tape/tape-page";export default function Page(){return <AppShell><main className="p-4 sm:p-5 lg:p-8"><TapePage/></main></AppShell>}
+import {AppShell} from "@/components/layout/app-shell";import {TapePage} from "@/components/tape/tape-page";export default function Page(){return <AppShell><main className="mx-auto w-full max-w-[1600px] p-4 sm:p-6 lg:p-8"><TapePage/></main></AppShell>}
